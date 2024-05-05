a121c8a1620be9933a97094d52c4b3a3c999f923
l_plaYT <pfihsnvifdbfgvfd@gmail.com>
2024-05-06 02:20:37 +0500

    Version 0.0.3-ALPHA-SNAPSHOT1

--- src/main/java/ru/lplay/storyscript/utils/ScriptReader.java
+++ src/main/java/ru/lplay/storyscript/utils/ScriptReader.java
@@ -4,19 +4,17 @@ import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ScriptReader {
 
     public static void readScript(String filePath, CommandContext<CommandSourceStack> context) {
         CommandSourceStack source = context.getSource();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF-8"));
             String line;
             while ((line = reader.readLine()) != null) {
-                execute(line, context);
+                msg(line, context);
             }
             reader.close();
         } catch (IOException e) {
@@ -24,8 +22,15 @@ public class ScriptReader {
         }
     }
 
-    private static void execute(String command, CommandContext<CommandSourceStack> context) {
+    private static void msg(String command, CommandContext<CommandSourceStack> context) {
         CommandSourceStack source = context.getSource();
-        source.sendSuccess(Component.nullToEmpty(command), true);
+        String[] parts = command.split("\"");
+        if (parts.length >= 3) {
+            String personage = parts[1];
+            String message = parts[2].substring(1).trim();
+            source.sendSuccess(Component.nullToEmpty("["+personage+"] "+message), true);
+        } else {
+            source.sendSuccess(Component.nullToEmpty("Неизвесная команда!"), true);
+        }
     }
 }

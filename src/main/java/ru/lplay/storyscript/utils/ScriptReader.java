29c2f52f10926bb988cfb35752099587d5e05431
l_plaYT <pfihsnvifdbfgvfd@gmail.com>
2024-05-06 18:13:28 +0500

    Version 0.0.3-ALPHA-SNAPSHOT2

--- src/main/java/ru/lplay/storyscript/utils/ScriptReader.java
+++ src/main/java/ru/lplay/storyscript/utils/ScriptReader.java
@@ -3,18 +3,26 @@ package ru.lplay.storyscript.utils;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class ScriptReader {
 
     public static void readScript(String filePath, CommandContext<CommandSourceStack> context) {
         CommandSourceStack source = context.getSource();
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF-8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8));
             String line;
             while ((line = reader.readLine()) != null) {
-                msg(line, context);
+                if(line.startsWith("msg"))
+                    msg(line, context);
+                else if(line.startsWith("setColor"))
+                    setColor(line, context);
+                else
+                    source.sendFailure(Component.literal("Неизвестная команда!").setStyle(Style.EMPTY.withColor(TextColor.parseColor("#d12a2a"))));
             }
             reader.close();
         } catch (IOException e) {
@@ -28,9 +36,14 @@ public class ScriptReader {
         if (parts.length >= 3) {
             String personage = parts[1];
             String message = parts[2].substring(1).trim();
-            source.sendSuccess(Component.nullToEmpty("["+personage+"] "+message), true);
-        } else {
-            source.sendSuccess(Component.nullToEmpty("Неизвесная команда!"), true);
+            Style style = Style.EMPTY.withColor(TextColor.parseColor("#ff0000"));
+            source.sendSuccess(Component.literal("["+personage+"] "+message).setStyle(style), true);
         }
+    }
+
+    private static void setColor(String command, CommandContext<CommandSourceStack> context) {
+        CommandSourceStack source = context.getSource();
+        source.sendSuccess(Component.literal("Установка цвета...").setStyle(Style.EMPTY.withColor(TextColor.parseColor("#7e8bfc"))), true);
+        // Тут будет код для установкие цвета... Потом
     }
 }

4f831df5a396178e737f8df5e4e5e2184ecc058b
l_plaYT <pfihsnvifdbfgvfd@gmail.com>
2024-05-11 14:53:48 +0500

    Version 1.3.2-ALPHA

--- src/main/java/ru/lplay/storyscript/utils/ScriptReader.java
+++ src/main/java/ru/lplay/storyscript/utils/ScriptReader.java
@@ -1,21 +1,28 @@
 package ru.lplay.storyscript.utils;
 
+import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+import static net.minecraft.commands.arguments.EntityArgument.getPlayer;
+import static ru.lplay.storyscript.utils.errorMsg.errorScript;
+
 public class ScriptReader {
 
-    private static String  msgColor = "#ffffff";
+    private static String msgColor = "#ffffff";
 
     public static void readScript(String filePath, CommandContext<CommandSourceStack> context) {
-        CommandSourceStack source = context.getSource();
         msgColor = "#ffffff";
+        CommandSourceStack source = context.getSource();
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8));
             String line;
@@ -24,11 +31,16 @@ public class ScriptReader {
                 lineNumber++;
                 if (!line.trim().isEmpty()) {
                     if (line.startsWith("msg"))
-                        msg(line, context);
+                        msg(line, source);
                     else if (line.startsWith("setColor"))
-                        setColor(line, context);
-                    else
-                        source.sendFailure(Component.literal("Неизвестная команда на "+lineNumber+" строчке.").setStyle(Style.EMPTY.withColor(TextColor.parseColor("#d12a2a"))));
+                        setColor(line);
+                    else if (line.startsWith("run"))
+                        runCmd(line, context);
+                    else if (line.startsWith("//")) {
+                    } else {
+                        errorScript("Неизвестная команда на " + lineNumber + " строчке.", context);
+                    }
+
                 }
             }
             reader.close();
@@ -37,22 +49,34 @@ public class ScriptReader {
         }
     }
 
-    private static void msg(String command, CommandContext<CommandSourceStack> context) {
-        CommandSourceStack source = context.getSource();
+    private static void msg(String command, CommandSourceStack source) {
+        MinecraftServer server = source.getServer();
         String[] parts = command.split("\"");
         if (parts.length >= 3) {
             String personage = parts[1];
             String message = parts[2].substring(1).trim();
             Style style = Style.EMPTY.withColor(TextColor.parseColor(msgColor));
-            source.sendSuccess((Component.literal("["+personage+"] ").setStyle(style)).append(Component.literal(message).setStyle(Style.EMPTY.withColor(TextColor.parseColor("#ffffff")))), true);
+            //source.sendSystemMessage((Component.literal("[" + personage + "] ").setStyle(style)).append(Component.literal(message).setStyle(Style.EMPTY.withColor(TextColor.parseColor("#ffffff")))));
+            for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                player.sendSystemMessage((Component.literal("[" + personage + "] ").setStyle(style)).append(Component.literal(message).setStyle(Style.EMPTY.withColor(TextColor.parseColor("#ffffff")))));
+            }
         }
     }
 
-    private static void setColor(String command, CommandContext<CommandSourceStack> context) {
-        CommandSourceStack source = context.getSource();
+    private static void setColor(String command) {
         String[] parts = command.split("\"");
         if (parts.length >= 2) {
             msgColor = parts[1];
+        }
+    }
+
+    public static void runCmd(String command, CommandContext<CommandSourceStack> context) {
+        CommandSourceStack source = context.getSource();
+        CommandDispatcher<CommandSourceStack> dispatcher = source.getServer().getCommands().getDispatcher();
+        try {
+            dispatcher.execute(command.substring(4), context.getSource());
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
         }
     }
 }
